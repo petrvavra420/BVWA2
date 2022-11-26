9fbf6bec2e9da16576c098eaf1c5287ec4e293aa
PetrVavraMl <petrvavraa@seznam.cz>
2022-11-26 18:22:37 +0100

    init

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+    <h1>REZERVAČNÍ SYSTÉM</h1>
+</header>
+
+<nav class="navigace">
+
+        <a href="">
+            Program
+        </a>
+
+
+        <a href="">
+            Pronájem
+        </a>
+
+        <a href="">
+            Kontakt
+        </a>
+   
+</nav>
+
+<main>
+
+</main>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
